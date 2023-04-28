95eadddb06d5a10cb99c648fc8b96ee20704df0c
Bentkowski <maciej.bentkowski@solera.com>
2023-04-28 13:45:21 +0200

    Added pricefile listing and possibility to choose from pricefiles
    - Added pricefile listing function
    - Added choose_pricefile function
    - If testing mode is True function is listing testing_pricefiles

--- modules/file_import.py
+++ modules/file_import.py
@@ -1,10 +1,38 @@
 from os import listdir
-from os.path import isfile, join
 
 TESTING_MODE_ON = True
 
 testing_pricefiles = listdir('./testing_pricefiles/')
 
+pricefiles_csv = []
+pricefiles_txt = []
+
+def pricefiles_listing():
+    pricefiles = listdir('./testing_pricefiles/')
+    pricefile_list = []
+    for file in pricefiles:
+        if file.endswith('.txt') or file.endswith('.csv'):
+            pricefile_list.append(file)
+    print(pricefile_list)
+    return pricefile_list
+
+def choose_pricefile():
+    pricefile_list = pricefiles_listing()
+    print('Choose a pricefile from the list above')
+    choice = input('Enter the file name: ')
+    if choice in pricefile_list:
+        return choice
+    else:
+        print('The file you have entered is not in the list above. Please try again')
+        choose_pricefile()
+
 if TESTING_MODE_ON:
-    for file in testing_pricefiles:
-        print(file)
+    pricefile_list = pricefiles_listing()
+    for file in pricefile_list:
+        if len(testing_pricefiles) > 1:
+            print('There are more than one pricefile in the testing_pricefiles directory. Please choose one')
+            choose_pricefile()
+            
+            exit()
+        else:
+            print(file)
