8a0ed92ab2487ef181f8be4e03b36f31bd1a6782
Bentkowski <maciej.bentkowski@solera.com>
2023-04-28 14:47:36 +0200

    Added possibility if Testing_mode is false and added gitignore
    -Deleted Pycache, i would find out for what is this file later

--- main.py
+++ main.py
@@ -12,7 +12,14 @@ if TESTING_MODE_ON:
         if len(pricefiles_list) > 1:
             print(pricefiles_list)
             print("There are more than one pricefile in the testing_pricefiles directory. Please choose one or type 'exit' to quit")
-            choose_pricefile(pricefiles_list)
+            choice = choose_pricefile(pricefiles_list)
+            print(choice)
             exit()
         else:
             print(file)
+else:
+    pricefiles_list = pricefiles_listing(pricefiles_directory)
+    choice = choose_pricefile(pricefiles_list)
+    print(choice)
+    exit()
+    
