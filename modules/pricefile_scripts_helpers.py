b50a0671b1ecac1c3ca60b585405cba4178db35f
Bentkowski <maciej.bentkowski@solera.com>
2023-05-09 11:44:07 +0200

    Added strange characters dictionary

--- modules/pricefile_scripts_helpers.py
+++ modules/pricefile_scripts_helpers.py
@@ -2,5 +2,67 @@
 
 def open_pricefile(pricefile):
     with open(pricefile, 'r', encoding='utf-8') as file:
-        for line in file:
-            print(line)
+        return file
+
+def strange_characters_replace(pricefile):
+    strange_characters_dictionary= {
+        ')': '',
+        '(': '',
+        'a': 'A',
+        'А': 'A',
+        'б': 'B',
+        'b': 'B',
+        'в': 'B',
+        'с': 'C',
+        'C': 'C',
+        'д': 'D',
+        'Д': 'D',
+        'd': 'D',
+        'E': 'E',
+        'э': 'E',
+        'е': 'E',
+        'f': 'F',
+        'ф': 'F',
+        'H': 'H',
+        'н': 'H',
+        'к': 'K',
+        'k': 'K',
+        'К': 'K',
+        'р': 'P',
+        'Р': 'P',
+        'п': 'P',
+        'П': 'P',
+        'т': 'T',
+        'Т': 'T',
+        't': 'T',
+        '0': '0',
+        'g': 'G',
+        'и': 'I',
+        'j': 'J',
+        'l': 'L',
+        'м': 'M',
+        'М': 'M',
+        'n': 'N',
+        'о': 'O',
+        'О': 'O',
+        'q': 'Q',
+        'л': 'L',
+        'y': 'Y',
+        'У': 'Y',
+        'u': 'U',
+        'Г': 'G',
+        'Ч': 'C',
+        'Ш': 'Z',
+        'З': 'Z',
+        'Ж': 'Z',
+        'Ą': 'A',
+        'Ć': 'C',
+        'Ź': 'Z',
+        'Ę': 'E',
+        'Ń': 'N',
+        ';': '.',
+        "'": '.',
+        '+': '-'
+
+    }
+
