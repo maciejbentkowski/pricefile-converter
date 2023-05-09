c22f98a14c59d3a7674268a4f6ec986e5578a126
Bentkowski <maciej.bentkowski@solera.com>
2023-05-09 10:43:56 +0200

    main script code simplyfy

--- main.py
+++ main.py
@@ -4,30 +4,19 @@ from modules.file_recognition import *
 #TURN ON TESTING MODE TO USE THE TESTING_PRICEFILES DIRECTORY
 TESTING_MODE_ON = True
 
-pricefiles_directory = './pricefiles/'
-
 if TESTING_MODE_ON:
     pricefiles_directory = './testing_pricefiles/'
-    pricefiles_list = pricefiles_listing(pricefiles_directory)
-    for file in pricefiles_list:
-        if len(pricefiles_list) > 1:
-            print(pricefiles_list)
-            print("There are more than one pricefile in the testing_pricefiles directory. Please choose one or type 'exit' to quit")
-            choice = choose_pricefile(pricefiles_list)
-            print(choice)
-            script_choice(pricefiles_directory, choice)
-            exit()
-        else:
-            print(file)
 else:
-    pricefiles_list = pricefiles_listing(pricefiles_directory)
-    for file in pricefiles_list:
-        if len(pricefiles_list) > 1:
-            print(pricefiles_list)
-            print("There are more than one pricefile in the testing_pricefiles directory. Please choose one or type 'exit' to quit")
-            choice = choose_pricefile(pricefiles_list)
-            print(choice)
-            script_choice(pricefiles_directory, choice)
-            exit()
-        else:
-            print(file)
+    pricefiles_directory = './pricefiles/'
+
+pricefiles_list = pricefiles_listing(pricefiles_directory)
+for file in pricefiles_list:
+    if len(pricefiles_list) > 1:
+        print(pricefiles_list)
+        print("There are more than one pricefile in the testing_pricefiles directory. Please choose one or type 'exit' to quit")
+        choice = choose_pricefile(pricefiles_list)
+        print(choice)
+        script_choice(pricefiles_directory, choice)
+        exit()
+    else:
+        print(file)
